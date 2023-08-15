a31a3bc9892004c4992b9134fc06e67c1b260bd0
Pedro H. F. Feitosa <pedrohenriqueff@gmail.com>
2023-08-15 20:17:14 -0300

    #3 ouput sink types

--- build.sbt
+++ build.sbt
@@ -5,4 +5,7 @@ scalaVersion := "2.13.11"
 libraryDependencies += "org.apache.spark" %% "spark-core" % "3.4.1"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.4.1"
 libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "3.4.1"
+// https://mvnrepository.com/artifact/mysql/mysql-connector-java
+libraryDependencies += "mysql" % "mysql-connector-java" % "8.0.33"
+
 //libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10_2.11" % "3.4.1"
---
+++ src/main/scala/sinks/ForEachBatch.scala
@@ -0,0 +1,68 @@
+package sinks
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Column, Dataset, Row, SparkSession}
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
+
+object ForEachBatch {
+
+  def getFileName: Column = {
+    val file_name = reverse(split(input_file_name(), "/")).getItem(0)
+    split(file_name, "_").getItem(0)
+  }
+
+  def saveToMySql = (df: Dataset[Row], batchId: Long) => {
+    val url = """jdbc:mysql://localhost:3306/training"""
+
+
+    df.withColumn("batchId", lit(batchId))
+      .write.format("jdbc")
+      .option("url", url)
+      .option("user", "root")
+      .option("password", "mysqlpass")
+      .option("dbtable", "training")
+      .mode("append")
+      .save()
+  }
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .appName("files source")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val schema = StructType(List(
+      StructField("Date", StringType, true),
+      StructField("Open", DoubleType, true),
+      StructField("High", DoubleType, true),
+      StructField("Low", DoubleType, true),
+      StructField("Close", DoubleType, true),
+      StructField("Adjusted Close", DoubleType, true),
+      StructField("Volume", DoubleType, true),
+    ))
+
+    val initDF = (
+      spark.readStream
+        .format("csv")
+        .option("maxFilesPerTrigger", 2)
+        .option("header", true)
+        .option("path", "./resources/data/stream")
+        .schema(schema)
+        .load()
+        .withColumn("Name", getFileName)
+      )
+
+    // TODO: Instalar lib para ler do mysql
+
+    val resultDf = initDF.select("Name", "Date", "Open")
+
+    resultDf
+      .writeStream
+      .outputMode("append")
+      .foreachBatch(saveToMySql)
+      .start()
+      .awaitTermination()
+  }
+}
